8503ddceffff15ae5738aacec7cdb42c7cdfe49e
Kamila Abdurakhmanova <cammiee.ace@gmail.com>
2025-03-08 01:46:20 +0600

    Добавлена локализация en

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:rick_and_morty/core/service/auto_router.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:rick_and_morty/core/service/di.dart' as get_it;
 
 final di = GetIt.instance;
@@ -18,6 +19,16 @@ class RickAndMortyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      localizationsDelegates: [
+    GlobalMaterialLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+  ],
+  supportedLocales: const [
+    Locale('ky'), 
+    Locale('en'), 
+    Locale('ru'), 
+  ],
       routerConfig: _appRouter.config(),
       debugShowCheckedModeBanner: false,
     );
